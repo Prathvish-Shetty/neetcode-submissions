92291339fc09e8627293e6e0f365bac95fbfad02
Prathvish Shetty <142135048+Prathvish-Shetty@users.noreply.github.com>
2026-07-19 22:27:30 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,29 @@
+class MinStack {
+    Stack<int[]> st;
+
+    public MinStack() {
+        st = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(st.isEmpty()){
+            st.push(new int[]{val, val});
+        } else{
+            st.push(new int[]{val, Math.min(val, st.peek()[1])});
+        }
+    }
+    
+    public void pop() {
+        if(!st.isEmpty()){
+            st.pop();
+        }
+    }
+    
+    public int top() {
+        return st.peek()[0];
+    }
+    
+    public int getMin() {
+        return st.peek()[1];
+    }
+}

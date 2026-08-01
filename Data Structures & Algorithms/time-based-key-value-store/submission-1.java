d1584354ee3f0de486eca9cdaa4536db9fd84398
Prathvish Shetty <142135048+Prathvish-Shetty@users.noreply.github.com>
2026-08-01 13:29:47 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.java
@@ -0,0 +1,33 @@
+class TimeMap {
+
+    public record Pair(String value, int timestamp){}
+
+    Map<String, List<Pair>> mp; 
+
+    public TimeMap() {
+        mp = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        Pair p = new Pair(value, timestamp);
+        if(!mp.containsKey(key)){
+            mp.put(key, new ArrayList<>());
+        }
+        mp.get(key).add(p);
+    }
+    
+    public String get(String key, int timestamp) {
+        List<Pair> pairs = mp.get(key);
+        if(pairs == null) return "";
+        int l = 0, h = pairs.size()-1;
+        String val = "";
+        while(l <= h){
+            int m = l+(h-l)/2;
+            if(pairs.get(m).timestamp() <= timestamp){
+                val = pairs.get(m).value();
+                l = m+1;
+            } else h = m-1;
+        }
+        return val;
+    }
+}

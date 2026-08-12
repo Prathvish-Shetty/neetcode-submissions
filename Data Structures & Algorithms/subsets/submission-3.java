279c571c260fb32bd017d48dcc48517c395f246d
Prathvish Shetty <142135048+Prathvish-Shetty@users.noreply.github.com>
2026-08-12 23:09:32 +0530

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    private void f(int[] nums, int i, List<Integer> al, List<List<Integer>> res){
+        res.add(new ArrayList<>(al));
+        for(int j = i; j < nums.length; j++){
+            al.add(nums[j]);
+            f(nums, j+1, al, res);
+            al.remove(al.size()-1);   
+        }
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        f(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+}
+/*
+Time:  O(n * 2^n)
+Space: O(n) recursion + O(n) current list
+       + O(n * 2^n) for the result
+*/
